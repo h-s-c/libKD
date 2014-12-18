b9bf7168e7e1feee9beeaa926dbf827ce95074e8
hsc <hsc@lapbox>
2014-12-18 02:09:43 +0100

    Fixed buildsystem for CLion IDE.

--- buildsystem/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export ROOT_DIR=$(pwd)/..
-export BUILDSYSTEM_DIR=$ROOT_DIR/buildsystem
-
-mkdir -p $BUILDSYSTEM_DIR/builds/debug
-cd $BUILDSYSTEM_DIR/builds/debug
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug -GNinja $BUILDSYSTEM_DIR
-ninja
