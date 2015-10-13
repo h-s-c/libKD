dafae49610dba7641d7836048c15339913953321
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2015-10-13 09:33:13 +0200

    Improved OpenGL ES SDK detection.

--- FindEGL.cmake
+++ FindEGL.cmake
@@ -1,6 +1,12 @@
-find_path(EGL_INCLUDE_DIR NAMES EGL/egl.h)
+find_path(EGL_INCLUDE_DIR NAMES EGL/egl.h PATHS ${CMAKE_SOURCE_DIR}/thirdparty/GLES_SDK/include)
+
 set(EGL_NAMES ${EGL_NAMES} egl EGL)
-find_library(EGL_LIBRARY NAMES ${EGL_NAMES})
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(ARCH_SUFFIX "-64")
+else()
+    set(ARCH_SUFFIX "")
+endif()
+find_library(EGL_LIBRARY NAMES ${EGL_NAMES} PATHS ${CMAKE_SOURCE_DIR}/thirdparty/GLES_SDK/x86${ARCH_SUFFIX})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(EGL DEFAULT_MSG EGL_LIBRARY EGL_INCLUDE_DIR)
--- FindKHR.cmake
+++ FindKHR.cmake
@@ -1,4 +1,4 @@
-find_path(KHR_INCLUDE_DIR NAMES KHR/khrplatform.h)
+find_path(KHR_INCLUDE_DIR NAMES KHR/khrplatform.h PATHS ${CMAKE_SOURCE_DIR}/thirdparty/GLES_SDK/include)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(KHR DEFAULT_MSG KHR_INCLUDE_DIR)
