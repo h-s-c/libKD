2dd065f2b8d939a6402b7a0c7b48fb0f005c6c12
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2015-10-13 09:39:02 +0200

    Improved OpenGL ES SDK detection.

--- FindGLES2.cmake
+++ FindGLES2.cmake
@@ -1,5 +1,11 @@
-find_path(GLES2_INCLUDE_DIR NAMES GLES2/gl2.h)
-find_library(GLES2_LIBRARY NAMES GLESv2)
+find_path(GLES2_INCLUDE_DIR NAMES GLES2/gl2.h PATHS ${CMAKE_SOURCE_DIR}/thirdparty/GLES_SDK/include)
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(ARCH_SUFFIX "-64")
+else()
+    set(ARCH_SUFFIX "")
+endif()
+find_library(GLES2_LIBRARY NAMES GLESv2 PATHS ${CMAKE_SOURCE_DIR}/thirdparty/GLES_SDK/x86${ARCH_SUFFIX})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GLES2 DEFAULT_MSG GLES2_LIBRARY GLES2_INCLUDE_DIR)
