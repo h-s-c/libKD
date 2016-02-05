841fb07adc6ed0de144bc20c315857084b83a897
hsc <hsc@hsc.hsc>
2016-02-05 01:41:16 +0100

    Changed CDash provider.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 set(CTEST_PROJECT_NAME "libKD")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 GMT")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "cdash-libkd.rhcloud.com")
 set(CTEST_DROP_LOCATION "/submit.php?project=libKD")
 set(CTEST_DROP_SITE_CDASH TRUE)
