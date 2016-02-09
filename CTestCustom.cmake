dec5987272e122a07efc7a3f5a3ebf2be07b8cec
hsc <hsc@hsc.hsc>
2016-02-09 18:44:03 +0100

    Improved ci build.

---
+++ CTestCustom.cmake
@@ -0,0 +1,11 @@
+set(CTEST_CUSTOM_COVERAGE_EXCLUDE
+    "/distribution/"
+    "/examples/"
+    "/thirdparty/"
+    "/tests/"
+    "/cov-int/"
+    "/CMakeFiles/"
+    "/usr/")
+
+set(CTEST_CUSTOM_ERROR_EXCEPTION
+    "Cannot find memory tester output file:")
