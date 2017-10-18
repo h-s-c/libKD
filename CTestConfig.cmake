49d131e29030b30725642dce23285f8b56a30e77
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2017-10-18 21:11:42 +0200

    Use new cdash host.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -27,6 +27,6 @@ set(CTEST_PROJECT_NAME "libKD")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 GMT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash-libkd.rhcloud.com")
-set(CTEST_DROP_LOCATION "/submit.php?project=libKD")
+set(CTEST_DROP_SITE "cdash.heliohost.org")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=libKD")
 set(CTEST_DROP_SITE_CDASH TRUE)
