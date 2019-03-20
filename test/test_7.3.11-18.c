2830755a9a40dae2c813f01a953f4189fbc16440
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2019-03-20 12:39:17 +0100

    Add more tests.

---
+++ test/test_7.3.11-18.c
@@ -0,0 +1,76 @@
+/******************************************************************************
+ * libKD
+ * zlib/libpng License
+ ******************************************************************************
+ * Copyright (c) 2014-2019 Kevin Schmidt
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ ******************************************************************************/
+
+#include <KD/kd.h>
+#include "test.h"
+
+#define THREAD_COUNT 10
+KDint done = 0;
+KDThreadCond *cond = KD_NULL;
+KDThreadMutex *mutex = KD_NULL;
+
+void *test_func(void *arg)
+{
+    kdThreadMutexLock(mutex);
+    done++;
+    kdThreadCondSignal(cond); 
+    kdThreadMutexUnlock(mutex);
+
+    return 0;
+}
+
+KDint KD_APIENTRY kdMain(KDint argc, const KDchar *const *argv)
+{
+    cond = kdThreadCondCreate(KD_NULL);
+    mutex = kdThreadMutexCreate(KD_NULL);
+
+    KDThread *threads[THREAD_COUNT] = {KD_NULL};
+    for(KDint i = 0; i < THREAD_COUNT; i++)
+    {
+        threads[i] = kdThreadCreate(KD_NULL, test_func, KD_NULL);
+        if(threads[i] == KD_NULL)
+        {
+            if(kdGetError() == KD_ENOSYS)
+            {
+                return 0;
+            }
+            TEST_FAIL();
+        }
+    }
+
+    kdThreadMutexLock(mutex);
+    while(done < THREAD_COUNT)
+    {
+        kdThreadCondWait(cond, mutex); 
+    }
+    kdThreadMutexUnlock(mutex);
+
+    for(KDint k = 0; k < THREAD_COUNT; k++)
+    {
+        kdThreadJoin(threads[k], KD_NULL);
+    }
+
+    kdThreadMutexFree(mutex);
+    kdThreadCondFree(cond);
+    return 0;
+}
