57636859c4920b60e6cbdea9f6e46994fa28fe06
Kevin Schmidt <kevin.patrick.schmidt@googlemail.com>
2021-07-13 23:13:10 +0200

    Disable test submission.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -26,7 +26,4 @@ set(CTEST_CMAKE_GENERATOR "Ninja")
 set(CTEST_PROJECT_NAME "libKD")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 GMT")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "cdash.crabdance.com")
-set(CTEST_DROP_LOCATION "/submit.php?project=libKD")
-set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_SUBMIT_URL "")
